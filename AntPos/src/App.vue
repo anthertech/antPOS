ddf7a4eca72a9b717ce6799aa263452779635d4e
NihalRoshanCK <nihalroshan55@gmail.com>
2025-05-21 13:06:03 +0530

    feat: remove app dialog component

--- AntPos/src/App.vue
+++ AntPos/src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
     <router-view />
-    <Dialogs />
     <FrappeUIProvider/>
   </div>
 </template>
 
 <script setup>
-  import { Dialogs } from './utils/ErrorDialog'
   import { FrappeUIProvider } from 'frappe-ui'
 </script>
