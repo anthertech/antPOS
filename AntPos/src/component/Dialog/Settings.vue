55daee0d4e74bbd62c82989d57e80c63de39b667
NihalRoshanCK <nihalroshan55@gmail.com>
2025-05-21 13:08:10 +0530

    feat: update settings dialog for dynamic branding

--- AntPos/src/component/Dialog/Settings.vue
+++ AntPos/src/component/Dialog/Settings.vue
@@ -6,19 +6,114 @@
                     <h1 class="px-2 pt-2 mb-3 text-lg font-semibold text-ink-gray-9">
                         {{ 'Settings' }}
                     </h1>
-                    
                 </div>
+                <div class="flex h-full flex-col gap-8 p-8 text-ink-gray-9">
+                    <h2 class="flex gap-2 text-xl font-semibold leading-none h-5">
+                        {{ 'General' }}
+                        <!-- <Badge
+                        v-if="settings.isDirty"
+                        :label="__('Not Saved')"
+                        variant="subtle"
+                        theme="orange"
+                        /> -->
+                    </h2>
+                    <div v-if="settings.doc" class="flex-1 flex flex-col gap-8 overflow-y-auto">
+                        <div class="flex w-full">
+                        <FormControl
+                            type="text"
+                            class="w-1/2"
+                            v-model="settings.doc.brand_name"
+                            :label="'Brand Name'"
+                        />
+                        </div>
+                        <div class="flex flex-col justify-between gap-4">
+                        <span class="text-base font-semibold text-ink-gray-9">
+                            {{ 'Logo' }}
+                        </span>
+                        <div class="flex flex-1 gap-5">
+                            <div
+                            class="flex items-center justify-center rounded border border-outline-gray-modals px-10 py-2"
+                            >
+                            <img
+                                :src="settings.doc?.brand_logo || '/assets/ant_pos/antPOS.png'"
+                                alt="Logo"
+                                class="size-8 rounded"
+                            />
+                            </div>
+                            <div class="flex flex-1 flex-col gap-2">
+                            <ImageUploader
+                                label="Favicon"
+                                image_type="image/ico"
+                                :image_url="settings.doc?.brand_logo"
+                                @upload="(url) => (settings.doc.brand_logo = url)"
+                                @remove="() => (settings.doc.brand_logo = '')"
+                            />
+                            <span class="text-p-sm text-ink-gray-6">
+                                Appears in the left sidebar. Recommended size is 32x32 px in PNG or SVG'
+                            </span>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="flex flex-col justify-between gap-4">
+                        <span class="text-base font-semibold text-ink-gray-9">
+                            {{'Favicon' }}
+                        </span>
+                        <div class="flex flex-1 gap-5">
+                            <div
+                            class="flex items-center justify-center rounded border border-outline-gray-modals px-10 py-2"
+                            >
+                            <img
+                                :src="settings.doc?.favicon || '/assets/ant_pos/antPOS.png'"
+                                alt="Favicon"
+                                class="size-8 rounded"
+                            />
+                            </div>
+                            <div class="flex flex-1 flex-col gap-2">
+                            <ImageUploader
+                                label="Favicon"
+                                image_type="image/ico"
+                                :image_url="settings.doc?.favicon"
+                                @upload="(url) => (settings.doc.favicon = url)"
+                                @remove="() => (settings.doc.favicon = '')"
+                            />
+                            <span class="text-p-sm text-ink-gray-6">
+                                Appears next to the title in your browser tab. Recommended size is 32x32 px in PNG or ICO',
+                                
+                            </span>
+                            </div>
+                        </div>
+                        </div>                         
+                    </div>
+                
+                    <div class="flex justify-between flex-row-reverse">
+                        <Button
+                        variant="solid"
+                        :label="'Update'"
+                        @click="updateSettings"
+                        />
+                        <ErrorMessage :message="settings.save.error" />
+                    </div>
+                    </div>
             </div>
         </template>
     </Dialog>
 </template>
 <script setup>
-    import { Dialog, Button, createListResource, createResource, TextInput, FeatherIcon } from 'frappe-ui';
+    import { Dialog, Button, createListResource, createResource, TextInput, FeatherIcon ,FormControl, Badge, ErrorMessage } from 'frappe-ui';
+    import ImageUploader from '../Controls/ImageUploader.vue'
     import { ref, inject, computed } from 'vue';
+    import { getSettings } from '../../stores/settings'
     import { createToast } from '../../utils';
     let base = inject('base');
     const dialogVisible = ref(true);
-    const selectedInvoice = ref(null);
-    const searchQuery = ref("");
     let errorHandled = false;
+    const { setting: settings, setupBrand } = getSettings()
+
+    function updateSettings() {
+    settings.save.submit(null, {
+        onSuccess: () => {
+        setupBrand()
+        },
+    })
+    }
 </script>
